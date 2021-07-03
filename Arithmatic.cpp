27f47f69748535df88393cf93e0fbf54afc550ee
ImtiazMahmod <imtiazmahmod2@gmail.com>
2021-07-03 22:40:04 +0600

    arithmathic

---
+++ Arithmatic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+    float a, b, sum;
+    cout<< "Add two numbers" <<endl;
+    cout<< "Input two number:";
+    cin>> a>>b;
+    sum=a+b;
+    cout << "Addition of two numbers:" << sum;
+    return 0;
+}
