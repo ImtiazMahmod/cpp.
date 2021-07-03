3437bb251a6c28063f34a1d94b1a0ba11758eea0
ImtiazMahmod <imtiazmahmod2@gmail.com>
2021-07-03 10:43:20 +0600

    AssignmentOperator

---
+++ AssignmentOperator.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+    //Assignment Operator
+    //= += -= *= /= %=
+    int x=6;
+
+    x+=6;
+    cout<<x<<endl ;
+
+    x-=6;
+    cout<<x<<endl;
+
+    x*=6;
+    cout<<x<<endl;
+
+    x/=6;
+    cout<<x<<endl;
+
+    x%=6;
+    cout<<x<<endl;
+
+
+    return 0;
+}
